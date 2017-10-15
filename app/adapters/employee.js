1c9c2763e16d9c176fab4a45616dbc8d4e5c02ac
PageB <PageB88@gmail.com>
2017-10-15 13:48:14 +0300

    Update adapter and default Ember calls

--- app/adapters/employee.js
+++ app/adapters/employee.js
@@ -1,55 +1,6 @@
 import DS from 'ember-data';
 
 export default DS.RESTAdapter.extend({
-  namespace: 'api',
-
-  // /**
-  //   Get all employees from API
-  //   @method download
-  //   @return {Promise} resolves to `RecordArray`
-  // */
-  // findAll() {
-  //   const empArray = {
-  //     "employee": [
-  //       {
-  //         "id": 1,
-  //         "externalId": 1,
-  //         "firstName": "John",
-  //         "lastName": "Owen",
-  //         "position": "UI Developer",
-  //         "team": "Mobile Team",
-  //         "startDate": "12.05.2016",
-  //         "birthDay": "05.08.1986"
-  //       },
-  //       {
-  //         "id": 2,
-  //         "externalId": 2,
-  //         "firstName": "Michael",
-  //         "lastName": "Carrick",
-  //         "position": "UI Developer",
-  //         "team": "Mobile Team",
-  //         "startDate": "12.05.2016",
-  //         "birthDay": "05.08.1986"
-  //       },
-  //       {
-  //         "id": 3,
-  //         "externalId": 3,
-  //         "firstName": "David",
-  //         "lastName": "Bradley",
-  //         "position": "UI Developer",
-  //         "team": "Mobile Team",
-  //         "startDate": "12.05.2016",
-  //         "birthDay": "05.08.1986"
-  //       }
-  //     ]
-  //   };
-
-  //   return empArray;
-  // },
-
-  findAll() {
-    const url = 'http://q1q1.eu/employees/employees/list';
-
-    return this.ajax(url);
-  }
+  // namespace: 'api',
+  host: 'http://q1q1.eu/employees/employees/list'
 });
