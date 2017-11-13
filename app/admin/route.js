4ab00662a0a21d71878582db84070599b68f7d14
PageB <PageB88@gmail.com>
2017-11-13 22:29:17 +0200

    Update authentication routes

--- app/admin/route.js
+++ app/admin/route.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
-import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
+import UnauthenticatedRouteMixin from 'ember-simple-auth/mixins/unauthenticated-route-mixin';
 
-export default Ember.Route.extend(ApplicationRouteMixin, {
+export default Ember.Route.extend(UnauthenticatedRouteMixin, {
   session: Ember.inject.service('session')
 });
