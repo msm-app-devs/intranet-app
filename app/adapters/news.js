a212fa459638305fd28d40fec6115ca03c75e29e
PageB <PageB88@gmail.com>
2017-12-05 23:02:20 +0200

    Extend news adapter and components (#8)

--- app/adapters/news.js
+++ app/adapters/news.js
@@ -1,8 +1,8 @@
 import DS from 'ember-data';
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
-export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
+export default DS.RESTAdapter.extend(DataAdapterMixin, {
     // namespace: 'api',
     authorizer: 'authorizer:oauth2',
-    host: 'http://q1q1.eu/news'
+    host: 'http://q1q1.eu/employees'
 });
