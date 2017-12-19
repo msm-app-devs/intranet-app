c7687a179ba3d41e58ebaf62b15efc64b39fbe57
svetoslav.bozhinov <sbojinov@yahoo.com>
2017-12-19 17:55:27 +0200

    customized adapters with localhost host

--- app/adapters/photo.js
+++ app/adapters/photo.js
@@ -4,5 +4,6 @@ import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 export default DS.RESTAdapter.extend(DataAdapterMixin, {
     // namespace: 'api',
     authorizer: 'authorizer:oauth2',
-    host: 'http://q1q1.eu/employees'
+    // host: 'http://q1q1.eu/employees'
+    host: 'http://localhost:80/intranet-api'
 });
--- app/adapters/token.js
+++ app/adapters/token.js
@@ -2,5 +2,6 @@ import DS from 'ember-data';
 
 export default DS.RESTAdapter.extend({
   // namespace: 'api',
-  host: 'http://q1q1.eu/token'
+  // host: 'http://q1q1.eu/token'
+  host: 'http://localhost:80/intranet-api/token'
 });
