b46c51b394c5da27392e18894f8dba4e42b7dae9
PageB <PageB88@gmail.com>
2018-01-03 21:38:52 +0200

    Update image upload

--- app/adapters/token.js
+++ app/adapters/token.js
@@ -2,6 +2,6 @@ import DS from 'ember-data';
 
 export default DS.RESTAdapter.extend({
   // namespace: 'api',
-  // host: 'http://q1q1.eu/token'
-  host: 'http://localhost:80/intranet-api/token'
+  host: 'http://q1q1.eu/admin'
+  // host: 'http://localhost:80/intranet-api/admin'
 });
