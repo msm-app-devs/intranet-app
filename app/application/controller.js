7c65b06353a0caafe59229a716cb0176a5cdd6c4
Martin Radev <Martin.Radev@fourth.com>
2018-02-06 10:03:41 +0200

    Remove computed property birthdayChecker

--- app/application/controller.js
+++ app/application/controller.js
@@ -1,9 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  birthdays: [],
-
-  birthdayCounter: Ember.computed('birthdays.[]', function() {
-    return this.get('birthdays').length;
-  })
+  birthdays: []
 });
