c59841b05b1db678d3af6d16e870b6a09eed6feb
PageB <PageB88@gmail.com>
2018-02-19 18:40:52 +0200

    Style benefits design

--- app/benefits/benefit/controller.js
+++ app/benefits/benefit/controller.js
@@ -1,5 +1,30 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-
+  files: [
+    {
+      fileName: 'DSK Bank Privite Banking Description',
+      fileDate: new Date(),
+      fileType: 'word',
+      fileSize: '61kb'
+    },
+    {
+      fileName: 'DSK Bank Privite Banking Description',
+      fileDate: new Date(),
+      fileType: 'powerpoint',
+      fileSize: '359kb'
+    },
+    {
+      fileName: 'DSK Bank Privite Banking Description',
+      fileDate: new Date(),
+      fileType: 'excel',
+      fileSize: '600kb'
+    },
+    {
+      fileName: 'DSK Bank Privite Banking Description',
+      fileDate: new Date(),
+      fileType: 'pdf',
+      fileSize: '600kb'
+    }
+  ],
 });
