c7385278db94b320fa7982ddcdc6315463c7be6e
svetoslav.bozhinov <sbojinov@yahoo.com>
2018-03-08 22:38:24 +0200

    Clear form button

--- app/components/news-form/component.js
+++ app/components/news-form/component.js
@@ -1,6 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  data: {}
+  data: {},
+
+    actions : {
+        test(){
+            const data = this.get('data');
+            // this.send('createNews',data);
+            // console.log(data);
+            this.set('data.title',null);
+            this.set('data.author',null);
+            this.set('data.date',null);
+            this.set('data.message',null);
+        }
+    }
 });
- 
