cbbb81a74aa2190b5cc947c056208e523dc286a6
Martin Radev <Martin.Radev@fourth.com>
2018-03-09 09:36:56 +0200

    Update news form save news and clear form

--- app/components/news-form/component.js
+++ app/components/news-form/component.js
@@ -3,15 +3,16 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   data: {},
 
-    actions : {
-        test(){
-            const data = this.get('data');
-            // this.send('createNews',data);
-            // console.log(data);
-            this.set('data.title',null);
-            this.set('data.author',null);
-            this.set('data.date',null);
-            this.set('data.message',null);
-        }
-    }
+  actions : {
+    /**
+      Send createNews action and clear employee form data.
+      @method createNews
+    */
+    createNews(){
+      const data = this.get('data');
+
+      this.sendAction('createNews', data);
+      this.set('data', {});
+    },
+  }
 });
