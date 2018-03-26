2a2c1519439c46b00aa67f367fb1c959da933b68
PageB <PageB88@gmail.com>
2018-03-26 23:59:23 +0300

    Disable fingerprint

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,6 +5,10 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
+    fingerprint: {
+      enabled: false
+    },
+
     sassOptions: {
       includePaths: ['app']
     },
