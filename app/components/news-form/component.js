0a2cf165c611d544db78102b1ffc04f31cf2f912
PageB <PageB88@gmail.com>
2018-05-02 22:05:09 +0300

    Update toolbar of tinyMCE

--- app/components/news-form/component.js
+++ app/components/news-form/component.js
@@ -10,7 +10,7 @@ export default Ember.Component.extend({
     height: 500,
     theme: 'modern',
     plugins: 'print preview fullpage searchreplace autolink directionality visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists textcolor wordcount imagetools contextmenu colorpicker textpattern help',
-    toolbar1: 'formatselect | bold italic strikethrough forecolor backcolor | link | alignleft aligncenter alignright alignjustify  | numlist bullist outdent indent  | removeformat',
+    toolbar1: 'formatselect | bold italic strikethrough forecolor backcolor | image | alignleft aligncenter alignright alignjustify  | numlist bullist outdent indent  | removeformat',
     image_advtab: true,
     // without images_upload_url set, Upload tab won't show up
     images_upload_url: 'http://q1q1.eu/employees/webroot/testimagenews.php',
