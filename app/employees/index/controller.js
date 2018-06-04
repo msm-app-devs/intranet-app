50df32f0746949508cb6d5b54f52737902614f69
PageB <PageB88@gmail.com>
2018-06-04 20:51:46 +0300

    Update employee-table-manager component

--- app/employees/index/controller.js
+++ app/employees/index/controller.js
@@ -3,29 +3,20 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   attrs: {},
 
+  /**
+   * Session service injection
+   *
+   * @property session
+   * @type SessionService
+   */
+  session: Ember.inject.service('session'),
+
   actions: {
-    filterByName(param) {
-      if (param !== '') {
-        const filterData = [];
-        this.get('store').peekAll('employee').toArray().forEach(employee => {
-          const fullName = employee.get('firstName').toLowerCase() + ' ' + employee.get('lastName').toLowerCase();
-
-          if (fullName.indexOf(param.toLowerCase()) !== -1) {
-            filterData.push(employee);
-          }
-        });
-
-        return filterData;
-      } else {
-        return this.get('store').peekAll('employee');
-      }
-    },
-
     filterByAll(param) {
       if (param !== '') {
         const filterData = [];
         this.get('store').peekAll('employee').toArray().forEach(employee => {
-          const fullDetails = employee.get('firstName').toLowerCase() + ' ' + employee.get('lastName').toLowerCase() + ' ' +  employee.get('firstName').toLowerCase() + ' ' + employee.get('lastName').toLowerCase() + ' ' + employee.get('position').toLowerCase() + ' ' + employee.get('team').toLowerCase() + ' ' + employee.get('company').toLowerCase() + ' ' + employee.get('education').toLowerCase() + ' ' + employee.get('expertise').toLowerCase() + ' ' + employee.get('skills').toLowerCase() + ' ' + employee.get('languages').toLowerCase() + ' ' + employee.get('hobbies').toLowerCase();
+          const fullDetails = employee.get('firstName').toLowerCase() + ' ' + employee.get('lastName').toLowerCase() + ' ' + employee.get('position').toLowerCase() + ' ' + employee.get('team').toLowerCase() + ' ' + employee.get('company').toLowerCase() + ' ' + employee.get('education').toLowerCase() + ' ' + employee.get('expertise').toLowerCase() + ' ' + employee.get('skills').toLowerCase() + ' ' + employee.get('languages').toLowerCase() + ' ' + employee.get('hobbies').toLowerCase();
 
           if (fullDetails.indexOf(param.toLowerCase()) !== -1) {
             filterData.push(employee);
