2de1db942ff94a9267aaabe25b2499936e16de08
Svetoslav <sbojinov@yahoo.com>
2018-06-21 01:14:42 +0300

    Change the drop down menus to be fulfilled from the API

--- app/adapters/position.js
+++ app/adapters/position.js
@@ -5,33 +5,33 @@ import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 // NB Delete findALl()
 // NB Delete findRecord()
 // export default DS.Adapter.extend({
-export default DS.Adapter.extend(DataAdapterMixin, {
+export default DS.RESTAdapter.extend(DataAdapterMixin, {
 // namespace: 'api',
-// authorizer: 'authorizer:oauth2',
-// host: 'http://localhost:80/employees'
+authorizer: 'authorizer:oauth2',
+host: 'http://localhost:80/employees'
 // host: 'http://localhost:80/intranet-api'
 
-  findAll() {
-    return [
-                {
-                    id: '1',
-                    optionName: 'PM'
-                },
-                {
-                    id: '2',
-                    optionName: 'Charter'
-                },
-                {
-                    id: '3',
-                    optionName: 'Scripter'
-                }
-            
-            ]
-  },
-  findRecord() {
-    return {
-                  id: '1',
-                  optionName: "PM"
-              };
-  }
+  // findAll() {
+  //   return [
+  //               {
+  //                   id: '1',
+  //                   optionName: 'PM'
+  //               },
+  //               {
+  //                   id: '2',
+  //                   optionName: 'Charter'
+  //               },
+  //               {
+  //                   id: '3',
+  //                   optionName: 'Scripter'
+  //               }
+  //
+  //           ]
+  // },
+  // findRecord() {
+  //   return {
+  //                 id: '1',
+  //                 optionName: "PM"
+  //             };
+  // }
 });
