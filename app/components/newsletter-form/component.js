799412d32d5f86f5f98ed006790352c845595743
Martin Radev <PageB88@gmail.com>
2018-07-13 22:50:12 +0300

    Add modal dialogs when adding (#21)

--- app/components/newsletter-form/component.js
+++ app/components/newsletter-form/component.js
@@ -2,8 +2,6 @@ import Component from '@ember/component';
 import Ember from 'ember';
 
 export default Component.extend({
-  data: {},
-
   categories: Ember.A([
     { categoryId: 1, name: 'Newsletter' }
   ]),
@@ -19,16 +17,8 @@ export default Component.extend({
       this.set('data.attachment', file)
     },
 
-    /**
-      Send createNewsletter action and clear newsletter form data property.
-      @method createNewsletter
-    */
-    createNewsletter(){
-      const data = this.get('data');
-
-      this.sendAction('createNewsletter', data);
-      // Ember.$('#file-field').val(null);
-      this.set('data', {});
+    showModalDialog(action) {
+      this.sendAction('showModalDialog', this.get('data'), action);
     }
   }
 });
